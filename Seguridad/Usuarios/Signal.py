9661689dcded23419f74435ee3126dd3a0c5d53f
Joshua27 <joshua.lopez23043219@estu.unan.edu.ni>
2026-05-14 17:09:16 -0600

    Configuracion de Errores 1.9

--- Seguridad/Usuarios/Signal.py
+++ Seguridad/Usuarios/Signal.py
@@ -1,3 +1,5 @@
+import threading
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
@@ -8,29 +10,26 @@ from django.utils.encoding import force_bytes
 from .models import Usuario
 
 
-# El decorador @receiver le dice a la función que "escuche" al modelo Usuario
+
 @receiver(post_save, sender=Usuario)
 def enviar_correo_activacion(sender, instance, created, **kwargs):
-    # IMPORTANTE: Solo queremos enviar el correo si el usuario es NUEVO (created = True)
-    # y si su rol es 'cliente' (no queremos mandarle esto a los administradores)
     if created and instance.rol == 'cliente':
-        # 1. Generamos el código criptográfico único para este usuario
         token = default_token_generator.make_token(instance)
-        # 2. Codificamos su ID para enviarlo seguro por la URL
         uid = urlsafe_base64_encode(force_bytes(instance.pk))
-
-        # 3. Armamos el enlace que el cliente va a clickear
-        # (Ese localhost cambiará por tu dominio real cuando subas la página a internet)
         enlace_activacion = f"https://web-production-93930.up.railway.app/apiUsuarios/Usuario/activar/{uid}/{token}/"
-
-        # 4. Redactamos y enviamos el correo
         asunto = "Confirma tu cuenta en ANGELA MUEBLERIA"
-        mensaje = f"Hola {instance.username},\n\nGracias por registrarte. Para activar tu cuenta y poder hacer pedidos, haz clic en el siguiente enlace:\n\n{enlace_activacion}"
-
-        send_mail(
-            asunto,
-            mensaje,
-            settings.EMAIL_HOST_USER,  # El correo de tu mueblería (configurado en settings.py)
-            [instance.email],  # El correo del cliente
-            fail_silently=False,
-        )
+        mensaje = f"Hola {instance.username},\n\nGracias por registrarte. Para activar tu cuenta, haz clic en el siguiente enlace:\n\n{enlace_activacion}"
+        # --- CAMBIO AQUÍ: Función interna para el hilo ---
+        def send_email_thread():
+            try:
+                send_mail(
+                    asunto,
+                    mensaje,
+                    settings.EMAIL_HOST_USER,
+                    [instance.email],
+                    fail_silently=False,
+                )
+            except Exception as e:
+                print(f"Error enviando correo: {e}")
+        # Iniciamos el hilo para que no bloquee el registro
+        threading.Thread(target=send_email_thread).start()
