3701eae3531220bfda791c530164d20017314b69
Joshua27 <joshua.lopez23043219@estu.unan.edu.ni>
2026-05-25 16:05:04 -0600

    temp: update debug_db to show users

--- APPS/Personalizacion/API/PersonalizacionAPI.py
+++ APPS/Personalizacion/API/PersonalizacionAPI.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
 from APPS.Personalizacion.models import MuebleBase, ColorMaterial
 from APPS.Personalizacion.API.SerializerPersonalizacion import SerializerMuebleBase, SerializerColorMaterial
 
@@ -9,6 +10,15 @@ class MuebleBaseViewsSet(ModelViewSet):
     queryset = MuebleBase.objects.all()
     serializer_class = SerializerMuebleBase
     permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['get'], permission_classes=[AllowAny])
+    def debug_db(self, request):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        users = list(User.objects.values('id', 'username', 'email', 'rol', 'is_superuser', 'is_staff', 'is_active'))
+        return Response({
+            "users": users
+        })
 
     def create(self, request, *args, **kwargs):
         if not request.user or request.user.is_anonymous or request.user.rol != 'admin':
